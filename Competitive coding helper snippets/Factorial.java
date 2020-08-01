cc9f03befeee0c343afc2bdbf25a3e8b7c9b8408
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-08-01 14:24:59 +0530

    added factorial code and series for cmpt. coding

---
+++ Competitive coding helper snippets/Factorial.java
@@ -0,0 +1,14 @@
+package Maths;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 30;
+        long result = 1;
+        
+        for(int i=1; i<=num; i++){
+            result *= i;
+        }
+        
+        // return result;
+    }
+}
